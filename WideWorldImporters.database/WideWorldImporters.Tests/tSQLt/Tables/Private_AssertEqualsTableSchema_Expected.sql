9287aa88adda947e4929583bdc025654608f94ec
Alex Yates <ajy.mail@gmail.com>
2017-09-06 13:42:03 +0100

    Now with tSQLt (no tests yet)

---
+++ WideWorldImporters.database/WideWorldImporters.Tests/tSQLt/Tables/Private_AssertEqualsTableSchema_Expected.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [tSQLt].[Private_AssertEqualsTableSchema_Expected] (
+    [name]           NVARCHAR (256) NULL,
+    [column_id]      INT            NULL,
+    [system_type_id] NVARCHAR (MAX) NULL,
+    [user_type_id]   NVARCHAR (MAX) NULL,
+    [max_length]     SMALLINT       NULL,
+    [precision]      TINYINT        NULL,
+    [scale]          TINYINT        NULL,
+    [collation_name] NVARCHAR (256) NULL,
+    [is_nullable]    BIT            NULL,
+    [is_identity]    BIT            NULL
+);
+
+
+
